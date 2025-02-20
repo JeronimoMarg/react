08adb70672255aff613df7abf4445862aea2ccc3
JeronimoMarg <jeronimomargitic@gmail.com>
2025-02-20 19:30:10 -0300

    Nueva funcionalidad con state siendo un solo objeto

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,34 @@ const Display = ({ counter }) => <div>{counter}</div>
 const Button = ({ onClick, texto }) => <button onClick={onClick}>{texto}</button>
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('Rendering App component')
+  const [clicks, setClicks] = useState({
+    left: 0, 
+    right: 0
+  })
 
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+  //Version no compacta
+  /*
+  const handleLeftClick = () => {
+    const newClicks = { 
+      ...clicks,
+      left: clicks.left + 1, 
+    }
+    setClicks(newClicks)
   }
+  */
 
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
+  const handleLeftClick = () => 
+    setClicks({...clicks, left: clicks.left + 1})
 
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
+  const handleRightClick = () =>
+    setClicks({...clicks, right: clicks.right + 1})
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} texto='plus'/>
-      <Button onClick={setToZero} texto='zero'/>
-      <Button onClick={decreaseByOne} texto='minus'/>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 }
