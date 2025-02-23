79b4bd6f2d4b9ac1f1dbec1fcbb73c636bd2106d
JeronimoMarg <jeronimomargitic@gmail.com>
2025-02-23 18:35:39 -0300

    Cambios que no me acuerdo

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,45 @@ import { useState } from 'react'
 
 const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = ({ onClick, texto }) => <button onClick={onClick}>{texto}</button>
+const Button = ({ onClick, texto }) => {
+  return (
+    <button onClick={onClick}>{texto}</button>
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, 
-    right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  //Version no compacta
-  /*
-  const handleLeftClick = () => {
-    const newClicks = { 
-      ...clicks,
-      left: clicks.left + 1, 
-    }
-    setClicks(newClicks)
-  }
-  */
+  const handleLeftClick = () => (setLeft(left + 1), setAll(allClicks.concat('L')), setTotal(total + 1))
 
-  const handleLeftClick = () => 
-    setClicks({...clicks, left: clicks.left + 1})
-
-  const handleRightClick = () =>
-    setClicks({...clicks, right: clicks.right + 1})
+  const handleRightClick = () => (setRight(right + 1), setAll(allClicks.concat('R')), setTotal(total + 1))
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {left}
+      <Button onClick={handleLeftClick} texto='left' /> 
+      <Button onClick={handleRightClick} texto='right' /> 
+      {right}
+      <History allClicks={allClicks}/>
+      <p>Total: {total}</p>
     </div>
   )
 }
