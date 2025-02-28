ee6f9a6d9f471e4397ae58377911b7b1018bf936
JeronimoMarg <jeronimomargitic@gmail.com>
2025-02-27 23:34:14 -0300

    Aplicacion de notas con POST, GET, PUT con service

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState, useEffect} from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
-const App = (props) => {
+const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('a new note...')
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
   const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    //buscar todas las notas con un GET mediante service
+    noteService
+      .getAll()
+      .then(data => {
+        console.log('promise fulfilled de traer todas las notas')
+        setNotes(data)
       })
   }
 
@@ -28,10 +29,16 @@ const App = (props) => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: String(notes.length + 1)
+      //id: String(notes.length + 1)  //el server organiza los id's automaticamente
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('a new Note...')
+    //hacer el post mediante el uso del service
+    noteService
+      .create(noteObject)
+      .then(data => {
+        console.log('post realizado: ', response)
+        setNotes(notes.concat(data))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -39,8 +46,26 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const handleToggleImportance = (note) => {
+    console.log('importance of note', note.id, 'needs to be toggled')
+    const url = `http://localhost:3001/notes/${note.id}`
+    //Se crea una copia del note porque no queremos modificar el objeto de forma directa. (NEVER MUTATE STATE DIRECTLY)
+    const changedNote = {...note, important: !note.important}
+    //usamos el servicio de notes para hacer el PUT
+    noteService
+      .update(note.id, changedNote)
+      .then(data => {
+        //Setear el array de notas con la nota modificada.
+        setNotes(notes.map(n => n.id === note.id ? data : n))
+      })
+      .catch(error => {
+        alert(`the note '${note.content}' was already deleted from server`)
+        setNotes(notes.filter(n => n.id !== note.id)) 
+      })
+  }
+
   const notesToShow = showAll ? notes : notes.filter (note => note.important === true)
-  const notesM = notesToShow.map(note => <Note key={note.id} note={note}></Note>)
+  const notesM = notesToShow.map(note => <Note key={note.id} note={note} toggleImportance={() => handleToggleImportance(note)}></Note>)
 
   return (
     <div>
